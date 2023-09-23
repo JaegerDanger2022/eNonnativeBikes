86238d3b7c08ef60131ff52c73c9a0c945a87975
JaegerDanger2022 <104800172+JaegerDanger2022@users.noreply.github.com>
2023-09-23 17:36:26 +0000

    update

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,8 +1,9 @@
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { ImageBackground, View } from "react-native";
 import React from "react";
 import ActionButton from "../components/ActionButton";
-import { Avatar } from "react-native-paper";
+import { Avatar, Text } from "react-native-paper";
 import { globalStyles } from "../constants/styles/global";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Welcome = ({ navigation }) => {
   return (
@@ -10,22 +11,41 @@ const Welcome = ({ navigation }) => {
       source={require("../../assets/images/welcomeImage.jpg")}
       style={{ height: "100%", width: "100%", ...globalStyles.container }}
     >
-      <View
-        style={{ flex: 2, flexDirection: "row", justifyContent: "flex-end" }}
-      >
-        <Avatar.Image
-          size={100}
-          source={require("../../assets/images/enonnativeLogo.png")}
-          style={{ backgroundColor: "white" }}
-        />
-      </View>
+      <SafeAreaView style={{ flex: 1, width: "100%" }}>
+        <View
+          style={{ flex: 2, flexDirection: "row", justifyContent: "flex-end" }}
+        >
+          <Avatar.Image
+            size={100}
+            source={require("../../assets/images/enonnativeLogo.png")}
+            style={{
+              backgroundColor: "white",
+              marginRight: 15,
+              marginTop: 10,
+            }}
+          />
+        </View>
 
-      <View style={{ flex: 5 }}>
-        <ActionButton
-          label={"Join Us"}
-          handleActionButton={() => navigation.navigate("SignUp")}
-        />
-      </View>
+        <View
+          style={{
+            flex: 5,
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <ActionButton
+            label={"Join Us"}
+            handleActionButton={() => navigation.navigate("SignUp")}
+          />
+        </View>
+        <View>
+          <Text>
+            By clicking 'Get Started you accept the user agreement and privacy
+            policy'
+          </Text>
+        </View>
+      </SafeAreaView>
     </ImageBackground>
   );
 };
