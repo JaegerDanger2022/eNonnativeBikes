a677574629e1d00c49f779c1e91984ad7c102b39
JaegerDanger2022 <mkmensahsol@yahoo.com>
2023-09-24 18:06:25 +0000

    Maps fixed

--- src/screens/MainStackScreens/Map.js
+++ src/screens/MainStackScreens/Map.js
@@ -1,13 +1,27 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Pressable } from "react-native";
 import React from "react";
+import CustomMap from "../../components/MapComponents/CustomMap";
+import { Ionicons } from "@expo/vector-icons";
 
-const Map = () => {
+const Map = ({ navigation }) => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Map</Text>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <CustomMap />
+        <View
+          style={{
+            position: "absolute",
+            zIndex: 10,
+            top: 80,
+            left: 16,
+          }}
+        >
+          <Pressable onPress={() => navigation.openDrawer()}>
+            <Ionicons name="menu-sharp" size={40} color="black" />
+          </Pressable>
+        </View>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
